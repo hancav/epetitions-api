3bba50bf9f6eb705601f341f8dd6c364ca8bce42
Hugo Cavalaria <hc@mail.pt>
2024-06-13 19:12:49 +0100

    saving a new petition

--- app/Models/Petition.php
+++ app/Models/Petition.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Petition extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'category',
+        'description',
+        'author',
+        'signees',
+    ];
+
 }
